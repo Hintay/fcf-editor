05eb5521637392a9e94223a5ba6a6c99cafdec75
Hintay <hintay@me.com>
2017-09-02 13:25:28 +0800

    Remove unused namespace.

--- src/FCF Editor/CollapsiblePanelDesigner.cs
+++ src/FCF Editor/CollapsiblePanelDesigner.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows.Forms.Design;
 using System.ComponentModel.Design;
-using System.Diagnostics;
 
 namespace FCF_Editor
 {
